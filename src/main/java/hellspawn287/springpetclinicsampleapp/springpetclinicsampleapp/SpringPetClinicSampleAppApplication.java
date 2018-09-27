9cc09da6914150af80815ebfa679e00972d414cb
Robert Prus <robert.prus87@gmail.com>
2018-09-28 00:16:44 +0200

    Initial Project Add

---
+++ src/main/java/hellspawn287/springpetclinicsampleapp/springpetclinicsampleapp/SpringPetClinicSampleAppApplication.java
@@ -0,0 +1,12 @@
+package hellspawn287.springpetclinicsampleapp.springpetclinicsampleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetClinicSampleAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPetClinicSampleAppApplication.class, args);
+    }
+}
---
+++ src/test/java/hellspawn287/springpetclinicsampleapp/springpetclinicsampleapp/SpringPetClinicSampleAppApplicationTests.java
@@ -0,0 +1,16 @@
+package hellspawn287.springpetclinicsampleapp.springpetclinicsampleapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringPetClinicSampleAppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
