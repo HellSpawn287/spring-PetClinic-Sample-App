2333f1e506f963ef41a6ed9bfc4597c4d14c2c18
Robert Prus <robert.prus87@gmail.com>
2018-10-04 02:25:38 +0200

    adding POJOs. Closes #2.

---
+++ src/test/java/hellspawn287/springpetclinicsampleapp/springpetclinicsampleapp/model/Owner.java
@@ -0,0 +1,4 @@
+package hellspawn287.springpetclinicsampleapp.springpetclinicsampleapp.model;
+
+public class Owner extends Person {
+}
---
+++ src/test/java/hellspawn287/springpetclinicsampleapp/springpetclinicsampleapp/model/PetType.java
@@ -0,0 +1,14 @@
+package hellspawn287.springpetclinicsampleapp.springpetclinicsampleapp.model;
+
+public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
