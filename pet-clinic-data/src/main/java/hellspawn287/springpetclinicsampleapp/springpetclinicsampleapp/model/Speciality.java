e5994de46f2eb6d46b48a8cf4432ec99cf168ea6
Robert Prus <robert.prus87@gmail.com>
2019-01-03 01:27:06 +0100

    Added specialities, added to vet. Closes #27

---
+++ pet-clinic-data/src/main/java/hellspawn287/springpetclinicsampleapp/springpetclinicsampleapp/model/Speciality.java
@@ -0,0 +1,14 @@
+package hellspawn287.springpetclinicsampleapp.springpetclinicsampleapp.model;
+
+public class Speciality extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
