bf2d20491fc2f323382098299130e0d9d4658685
Robert Prus <robert.prus87@gmail.com>
2019-01-03 02:33:34 +0100

    Added PetTypeMapService and PetTypeService. Closes #29

---
+++ pet-clinic-data/src/main/java/hellspawn287/springpetclinicsampleapp/springpetclinicsampleapp/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package hellspawn287.springpetclinicsampleapp.springpetclinicsampleapp.services;
+
+import hellspawn287.springpetclinicsampleapp.springpetclinicsampleapp.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
