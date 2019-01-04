dcfaec05b5d7321aa442436747f4955d7b1b0012
Robert Prus <robert.prus87@gmail.com>
2019-01-04 03:57:45 +0100

    Fixed broken links. Closes #46

--- pet-clinic-web/src/main/java/hellspawn287/springpetclinicsampleapp/springpetclinicsampleapp/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/hellspawn287/springpetclinicsampleapp/springpetclinicsampleapp/controllers/OwnerController.java
@@ -4,6 +4,7 @@ import hellspawn287.springpetclinicsampleapp.springpetclinicsampleapp.services.O
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
@@ -15,8 +16,14 @@ public class OwnerController {
     }
 
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners(Model model){
+    public String listOwners(Model model) {
         model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
+    }
+
+    @RequestMapping("/find")
+    public String findOwners() {
+
+        return "notimplemented";
     }
 }
