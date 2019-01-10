dd3521590efa15d896393ad4271db4bce1c39346
Robert Prus <robert.prus87@gmail.com>
2019-01-10 04:34:20 +0100

    Added Visit Service Implemented Map Service for Visit Service. Close #47

---
+++ pet-clinic-data/src/main/java/hellspawn287/springpetclinicsampleapp/springpetclinicsampleapp/services/VisitService.java
@@ -0,0 +1,6 @@
+package hellspawn287.springpetclinicsampleapp.springpetclinicsampleapp.services;
+
+import hellspawn287.springpetclinicsampleapp.springpetclinicsampleapp.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
